22eecda59481905ff4061161cef6873c307fc535
chidrome <chidrome@gmail.com>
2019-05-08 10:42:20 -0700

    added component did mount function on map component. searchHandler in search component

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
+import superagent from 'superagent';
+import SERVER_URL from './constant/server';
 import Search from './Search.js';
 import Map from './Map.js';
 import Result from './Result.js';
-import superagent from 'superagent';
+
 
 export default class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
       long: 0,
-      lat: 0
+      lat: 0,
     }
   }
 
+  componentDidMount = () => {
+    this.getLocation();
+  }
+
+  getLocation = () => {
+    superagent.get(`${SERVER_URL}/location`)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => {
+      console.log(`There was an error with your request to the backend ${error}`)
+    });
+  }
+  
 
 
   render() {
