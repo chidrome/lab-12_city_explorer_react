dd6417ffd32d844ee4412f937bb8d8fd2d440401
chidrome <chidrome@gmail.com>
2019-05-09 15:38:57 -0700

    changing server address to the deployed site

--- src/constant/server.js
+++ src/constant/server.js
@@ -1,2 +1,2 @@
-export default 'http://localhost:5000';
-// export default 'https://stormy-tundra-35915.herokuapp.com'
+// export default 'http://localhost:5000';
+export default 'https://stormy-tundra-35915.herokuapp.com'
